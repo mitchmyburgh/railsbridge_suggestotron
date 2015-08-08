3cdb8970595af83e9221ceb4632835d34cf2252c
Mitch Myburgh <mitchmyburgh@gmail.com>
2015-08-08 15:07:42 +0200

    attaempt at about page

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,9 @@
 class Topic < ActiveRecord::Base
-	has_many :votes, dependent: :destroy
+	has_many :votes, dependent: :destroy#,:order => "votes.count DESC"
+
+	def self.sorted
+		#Topic.all
+		Topic.all.sort_by{|topic| topic.votes.count}.reverse
+		#Topic.all
+	end
 end
